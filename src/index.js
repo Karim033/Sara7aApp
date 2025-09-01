a72d31a200a0a272e8effa5e5787a8bb54ba122f
Karim033 <gaberk033@gmail.com>
2025-09-01 15:47:22 +0300

    Refactor code structure for improved readability and maintainability

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import bootstrap from "./app.controller.js";
+import chalk from "chalk";
+const app = express();
+const port = process.env.PORT || 5000;
+
+await bootstrap(app, express);
+
+app.listen(port, () =>
+  console.log(
+    chalk.bgGreen(chalk.black(`Example app listening on port ${port}!`))
+  )
+);
